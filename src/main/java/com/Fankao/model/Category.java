982bde16eff61c7f60c2ad13826be1671dd31807
2019211001000821-Fankao <79890540+2019211001000821-Fankao@users.noreply.github.com>
2021-05-16 15:41:13 +0800

    11.HW.week11 Commit

---
+++ src/main/java/com/Fankao/model/Category.java
@@ -0,0 +1,98 @@
+package com.Fankao.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Category {
+    private int categoryId;
+    private String categoryName;
+    private String description;
+    private boolean active;
+
+    public Category(){
+
+    }
+
+    public Category(int categoryId, String categoryName, String description, boolean active) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.description = description;
+        this.active = active;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryId=" + categoryId +
+                ", categoryName='" + categoryName + '\'' +
+                ", description='" + description + '\'' +
+                ", active=" + active +
+                '}';
+    }
+    public static List<Category> findAllCategory(Connection con){
+        List<Category> list =new ArrayList<Category>();
+        String queryString = "select * from Category";
+        try {
+            PreparedStatement statement = con.prepareStatement(queryString);
+            ResultSet resultSet = statement.executeQuery();
+            while(resultSet.next()){
+                Category c = new Category();
+                c.setCategoryId(resultSet.getInt("categoryId"));
+                c.setCategoryName(resultSet.getString("categoryName"));
+                c.setDescription(resultSet.getString("Description"));
+                list.add(c);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return list;
+
+    }
+    public static String findByCategory(Connection con,int categoryId) throws SQLException {
+        String sql = "select * from Category while categoryId=?";
+        PreparedStatement pt = con.prepareStatement(sql);
+        pt.setInt(1,categoryId);
+        ResultSet rs =pt.executeQuery();
+        String categoryName = null;
+        while (rs.next()){
+            categoryName = rs.getString("categoryName");
+        }
+        return categoryName;
+    }
+}
